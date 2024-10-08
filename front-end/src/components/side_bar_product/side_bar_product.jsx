0553841eb7b64d21137996d348021d59bde0c43e
DO NGOC HIEU <122579807+DoNgocHieu@users.noreply.github.com>
2024-10-09 01:53:41 +0700

    Done show grid products list UI + data

---
+++ front-end/src/components/side_bar_product/side_bar_product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBarProduct = () => {
+    return (
+        <div style={{ background: 'red', width: '100%', height: '100%' }}>
+            <h1>SideBarProduct</h1>
+        </div>
+    );
+}
+
+export default SideBarProduct;
