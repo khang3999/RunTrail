b33d47a55293823cd58879c262eca836dff8a805
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-10-08 17:00:44 +0700

    Done product item UI

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import addToCartBtn from '@/assets/css/addToCartBtn.css'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
