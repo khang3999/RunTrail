6bb3901877bb16a88e3e9cdb81aa9075d9346501
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-10-20 11:16:32 +0700

    done ui an function

--- front-end/src/components/detail/ImageDesktop.jsx
+++ front-end/src/components/detail/ImageDesktop.jsx
@@ -5,7 +5,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
+import '@/assets/css/imageDesktop.css'
+import ReactImageMagnify from 'react-image-magnify';
 export default function ImageDesktop() {
     const perPage = 5
     const data = [
@@ -14,28 +15,37 @@ export default function ImageDesktop() {
         'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-03.jpg',
         'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-04.jpg',
         'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
     ]
     return (
-        <div className='w-auto mx-20 flex-row'>
+        <div className='flex w-auto mx-20 flex-row'>
             {/* Image section */}
-            <div className='flex-1 flex flex-col w-1/2 px-10 py-4'>
+            <div className='flex flex-col w-1/2 px-10 py-4'>
                 {/* Hover image */}
                 <div className='w-full mb-8 flex justify-center items-center'>
-                    {/* <ReactImageMagnify {...{
-    smallImage: {
-      alt: 'Wristwatch by Ted Baker London',
-      isFluidWidth: true,
-      src: "https://pos.nvncdn.com/be3294-43017/ps/20230411_VmJ2SfyO69.jpeg"
-    },
-    largeImage: {
-      src: "https://pos.nvncdn.com/be3294-43017/ps/20230411_VmJ2SfyO69.jpeg",
-      width: 1200,
-      height: 1800
-    },
-    // enlargedImageStyle:{
-    //   objectFit:'contain'
-    // }
-  }} /> */}
+                    <ReactImageMagnify {...{
+                        smallImage: {
+                            alt: 'Wristwatch by Ted Baker London',
+                            isFluidWidth: true,
+                            src: "https://pos.nvncdn.com/be3294-43017/ps/20230411_VmJ2SfyO69.jpeg"
+                        },
+                        largeImage: {
+                            src: "https://pos.nvncdn.com/be3294-43017/ps/20230411_VmJ2SfyO69.jpeg",
+                            width: 1200,
+                            height: 1200
+                        },
+                        enlargedImageContainerDimensions: {
+                            width: '120%',
+                            height: '120%'
+                        },
+                        enlargedImageContainerStyle: {
+                            marginLeft: 25
+                        }
+                        
+
+                    }} />
+
                 </div>
 
                 {/* List images slide*/}
@@ -51,7 +61,7 @@ export default function ImageDesktop() {
                     >
                         {data.map((image, index) => {
                             return (
-                                <SwiperSlide key={index} className='bg-green-400 border-[1px]'><button onClick={() => console.log("press")}><img src={image}></img></button></SwiperSlide>
+                                <SwiperSlide key={index} className='bg-green-400 border-[1px]'><button onClick={() => console.log("press")}><img style={{ maxWidth: '100%' }} src={image}></img></button></SwiperSlide>
                             )
                         })}
                     </Swiper>
@@ -59,7 +69,7 @@ export default function ImageDesktop() {
             </div>
             {/* <ImageDesktopSkeleton></ImageDesktopSkeleton> */}
             {/* Infor section */}
-            <div className='flex-1 bg-orange-300 h-[300px] w-1/2'></div>
+            <div className='bg-orange-300 h-[300px] w-1/2'></div>
         </div>
     )
 }
