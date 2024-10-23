8a391fed12d501793eb8452d36b2a5e0ab345bcc
Datto098 <dat61222@gmail.com>
2024-10-23 15:00:33 +0700

    UPDATE:: update related product category

--- front-end/src/app/product/[slug]/page.jsx
+++ front-end/src/app/product/[slug]/page.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 import ProductDetailItem from '@/components/ProductDetailItem';
 import Overplay from '@/components/Overlay';
 import ProductImageModal from '@/components/ProductImageModal';
+import RelatedProduct from '@/components/RelatedProduct';
 export default function DetailProduct() {
 	const [showModal, setShowModal] = useState(false);
 	const [product, setProduct] = useState({
 		spuName: '',
 		images: [],
+		categoryId: '',
 		brand: {
 			brandName: '',
 		},
@@ -27,12 +29,14 @@ export default function DetailProduct() {
 		const data = await response.json();
 		if (data.statusCode === 200) {
 			console.log(data.metadata);
-			const { spuName, brand, spuAttributes, images } = data.metadata;
+			const { spuName, brand, spuAttributes, images, categoryId } =
+				data.metadata;
 
 			setProduct({
 				spuName,
 				brand,
 				images,
+				categoryId,
 				spuAttributes: JSON.parse(spuAttributes),
 			});
 			setIsLoading(false);
@@ -74,7 +78,7 @@ export default function DetailProduct() {
 			</div>
 			<div className="px-[200px] mt-3 h-[300px] grid grid-cols-2 gap-4">
 				{/* product images */}
-				<div className="bg-slate-200">
+				<div className="">
 					<button
 						onClick={() => {
 							if (product.images && product.images.length > 0) {
@@ -85,6 +89,7 @@ export default function DetailProduct() {
 						}}
 					>
 						<img
+							className="w-full h-full object-cover"
 							src={
 								product.images && product.images.length > 0
 									? product.images[0].imgUrl
@@ -103,6 +108,12 @@ export default function DetailProduct() {
 						isLoading={isLoading}
 					/>
 				</div>
+			</div>
+			<div className="mt-[300px]">
+				<RelatedProduct
+					categories={product.categoryId}
+					isLoading={isLoading}
+				/>
 			</div>
 		</div>
 	);
