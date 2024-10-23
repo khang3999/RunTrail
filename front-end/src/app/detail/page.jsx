c75f76032877d3a80df4af5454c1811292dfa499
charcasbeos <tranthianhthu0711@gmail.com>
2024-10-23 12:28:26 +0700

    no message

--- front-end/src/app/detail/page.jsx
+++ front-end/src/app/detail/page.jsx
@@ -2,8 +2,10 @@
 import TabInformation from '@/components/TabInformation'
 import TabInformationSkeleton from '@/components/TabInformationSkeleton';
 import { useProductProvider } from '@/contexts/ProductProvider';
+import logo_shop from '@/assets/images/logo_shop.png'
 import React from 'react'
-
+import ReactImageMagnify from 'react-image-magnify';
+import '@/assets/css/style.css'
 export default function DetailProduct() {
   const {
     products,
@@ -30,7 +32,7 @@ export default function DetailProduct() {
 
       <TabInformation ></TabInformation>
 
-
     </div>
+
   )
 }
