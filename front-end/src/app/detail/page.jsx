16f0c965a7dca4b44a6acea91175d17c11ffa4ff
charcasbeos <tranthianhthu0711@gmail.com>
2024-10-24 14:33:40 +0700

    Done Tab Information

--- front-end/src/app/detail/page.jsx
+++ front-end/src/app/detail/page.jsx
@@ -1,11 +1,9 @@
 'use client'
-import TabInformation from '@/components/TabInformation'
 import TabInformationSkeleton from '@/components/TabInformationSkeleton';
 import React from 'react'
 import RelatedProduct from '@/components/RelatedProduct'
 import { useProductProvider } from '@/contexts/ProductProvider';
 import logo_shop from '@/assets/images/logo_shop.png'
-import React from 'react'
 import ReactImageMagnify from 'react-image-magnify';
 import '@/assets/css/style.css'
 export default function DetailProduct() {
@@ -18,21 +16,10 @@ export default function DetailProduct() {
     totalPages,
     currentProducts,
   } = useProductProvider();
-  if (isLoading) {
-    return (
-      <div>
-        <div >
-
-          <TabInformationSkeleton />
-
-        </div>
-      </div>
-    );
-  }
+  
   return (
     <div>DetailProduct
 
-      <TabInformation ></TabInformation>
 
       <RelatedProduct categories={8} isLoading={isLoading} />
 
--- front-end/src/app/product/[slug]/page.jsx
+++ front-end/src/app/product/[slug]/page.jsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useEffect, useState } from 'react'
 import ProductDetailItem from '@/components/ProductDetailItem';
+import TabInformation from '@/components/TabInformation';
+import RelatedProduct from '@/components/RelatedProduct';
 
 export default function DetailProduct() {
 
@@ -9,6 +11,8 @@ export default function DetailProduct() {
     brand: {
       brandName: ''
     },
+    categoryId:'',
+    spuDescription:'',
     spuAttributes: {}
   });
   const [isLoading, setIsLoading] = useState(true);
@@ -21,13 +25,15 @@ export default function DetailProduct() {
 
   const fetchProductDetail = async () => {
     setIsLoading(true)
-    const response = await fetch('http://localhost:8008/api/v1/spu?id=2')
+    const response = await fetch('http://localhost:8008/api/v1/spu?id=42')
     const data = await response.json()
     if (data.statusCode === 200) {
-      const { spuName, brand, spuAttributes } = data.metadata;
+      const { spuName, brand, spuAttributes, spuDescription,categoryId } = data.metadata;
       setProduct({
         spuName,
         brand,
+        spuDescription,
+        categoryId,
         spuAttributes: JSON.parse(spuAttributes)
       });
       setIsLoading(false)
@@ -39,8 +45,8 @@ export default function DetailProduct() {
   }
 
   return (
-    <div>
-      <div className='px-[200px] mt-3 h-[300px] grid grid-cols-2 gap-4'>
+    <div className='pb-[200px]'>
+      <div className='px-[200px] mt-3 h-[300px] grid grid-cols-2 gap-4 mb-[200px]'>
         {/* product images */}
         <div className='bg-slate-200'>
         </div>
@@ -48,6 +54,10 @@ export default function DetailProduct() {
           <ProductDetailItem product={product} isLoading={isLoading} />
         </div>
       </div>
+      <TabInformation product={product} isLoading={isLoading}></TabInformation>
+
+      <RelatedProduct categories={product.categoryId} isLoading={isLoading} />
+
     </div>
   )
 }
