ead73fc88922cb6f691f4f29282c090d64274ff2
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-10-24 10:24:50 +0700

    Completed swiper and image magnify

--- front-end/src/components/detail/ImageDesktop.jsx
+++ front-end/src/components/detail/ImageDesktop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -18,34 +18,37 @@ export default function ImageDesktop() {
         'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
         'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
     ]
+    const [selectedImage, setSelectedImage] = useState(data[0])
+    const handleThumnailClick = (image)=>{
+        setSelectedImage(image);
+    }
     return (
         <div className='flex w-auto mx-20 flex-row'>
             {/* Image section */}
-            <div className='flex flex-col w-1/2 px-10 py-4'>
+            <div className='flex flex-col w-1/2 px-10 py-4 border-2 justify-center items-center'>
                 {/* Hover image */}
-                <div className='w-full mb-8 flex justify-center items-center'>
+                <div className='w-[80%] mb-8 flex justify-center items-center z-[3]'>
                     <ReactImageMagnify {...{
                         smallImage: {
                             alt: 'Wristwatch by Ted Baker London',
-                            isFluidWidth: true,
-                            src: "https://pos.nvncdn.com/be3294-43017/ps/20230411_VmJ2SfyO69.jpeg"
+                            // isFluidWidth: true,
+                            width: 300,
+                            height: 400,
+                            src: selectedImage
                         },
                         largeImage: {
-                            src: "https://pos.nvncdn.com/be3294-43017/ps/20230411_VmJ2SfyO69.jpeg",
+                            src: selectedImage,
                             width: 1200,
-                            height: 1200
+                            height: 1800
                         },
                         enlargedImageContainerDimensions: {
-                            width: '120%',
-                            height: '120%'
+                            width: '140%',
+                            height: '130%'
                         },
                         enlargedImageContainerStyle: {
-                            marginLeft: 25
+                            marginLeft: 130
                         }
-                        
-
                     }} />
-
                 </div>
 
                 {/* List images slide*/}
@@ -61,7 +64,7 @@ export default function ImageDesktop() {
                     >
                         {data.map((image, index) => {
                             return (
-                                <SwiperSlide key={index} className='bg-green-400 border-[1px]'><button onClick={() => console.log("press")}><img style={{ maxWidth: '100%' }} src={image}></img></button></SwiperSlide>
+                                <SwiperSlide key={index} className='bg-green-400 border-[1px]'><button onClick={()=>handleThumnailClick(image)}><img style={{ maxWidth: '100%' }} src={image}></img></button></SwiperSlide>
                             )
                         })}
                     </Swiper>
