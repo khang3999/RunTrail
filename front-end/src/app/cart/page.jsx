70453aa4a6e65db595b4f89bdd8cb45973045a19
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-11-09 13:43:11 +0700

    no message

--- front-end/src/app/cart/page.jsx
+++ front-end/src/app/cart/page.jsx
@@ -1,12 +1,16 @@
-import { InputNumber } from 'antd'
+import { IoTrashOutline } from "react-icons/io5";
+import { Button, InputNumber, Tooltip } from 'antd'
 import React from 'react'
 
 export default function CartPage() {
   return (
     <>
-      <div>CartPage</div>
-      <InputNumber>
-      </InputNumber>
+      <div>CartPage 
+      <Tooltip title="Xóa" color="red">
+        <Button danger type="primary" shape="default" icon={<IoTrashOutline size={22} />} />
+      </Tooltip>
+      </div>
+      
     </>
 
   )
