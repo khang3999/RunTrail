9263276df43942fc7bf27d39106cea0743fd9ff8
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-11-10 15:17:33 +0700

    FIX:: slug in dynamic page use generateStaticParams

--- front-end/src/app/product/[slug]/page.jsx
+++ front-end/src/app/product/[slug]/page.jsx
@@ -1,18 +1,18 @@
 import ProductDetail from "@/components/ProductDetail";
 import PageTitle from "@/components/PageTitle";
-export function generateStaticParams(){
-  return [
-      {
-          slug: "adidas-superstar",
-      }
-  ]
+import AxiosInstance from '@/utils/axiosInstance';
+export async function generateStaticParams() {
+  const {metadata:slugs} = await fetch('http://localhost:8008/api/v1/spu/all-slug').then((res) => res.json())
+  return slugs.map((slug) => ({
+    slug
+  })); 
 }
 
-export default function ProductDetailPage({params}) {
-const {slug} = params;
-return (
-  <PageTitle title={"Product Detail"}>
-    <ProductDetail slug={slug} />
-  </PageTitle>
-);
+export default async function ProductDetailPage({ params }) {
+  const { slug } = await params;
+  return (
+    <PageTitle title={"Product Detail"}>
+      <ProductDetail slug={slug} />
+    </PageTitle>
+  );
 }
