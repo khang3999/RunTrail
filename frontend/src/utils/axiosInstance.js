1286ec0eeb503cd798d881b68b265b2665e426f1
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-12-14 09:16:17 +0700

    UPDATE: endpoint

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 // Axios Interceptor Instance
-const baseURL = process.env.BASE_URL
-  ? process.env.BASE_URL
-  : "https://d1cbfvoaqkpuni.cloudfront.net/api/v1";
+const baseURL = "https://d1cbfvoaqkpuni.cloudfront.net/api/v1";
 
 const AxiosInstance = axios.create({
   baseURL,
